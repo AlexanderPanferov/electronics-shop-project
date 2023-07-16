61b550c3b7ff7764316e304c4580dececdb93c93
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-07-16 11:07:39 +0700

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,14 +6,21 @@ from src.item import Item
 def item():
     return Item("Test Item", 10, 5)
 
+
 def test_init(item):
     assert item.name == "Test Item"
     assert item.price == 10
     assert item.quantity == 5
 
+
 def test_calculate_total_price(item):
     assert item.calculate_total_price() == 50
+
 
 def test_apply_discount(item):
     item.apply_discount()
     assert item.price == 10
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5.0') == 5
