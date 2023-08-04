ef65877c21f4ce6a6ec6f4f59c614ca65acbe09a
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-08-04 20:30:59 +0700

    corrected file names

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,9 +2,9 @@ from src.items import Item
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv('test1.csv')
+    Item.instantiate_from_csv('missing_file')
     # FileNotFoundError: Отсутствует файл items.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv('test2.csv')
+    Item.instantiate_from_csv('corrupted_file.csv')
     # InstantiateCSVError: Файл items.csv поврежден
